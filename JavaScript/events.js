645453705f564d294a00e7c69305047619c731d6
Atom Mayilyan <mayilyan.atom@mail.ru>
2026-07-31 14:39:42 +0300

    Countdown time day 1

--- JavaScript/events.js
+++ JavaScript/events.js
@@ -111,4 +111,63 @@ function changeTransitionDuration(event)
 	// Լրացուցիչ՝ console-ում ցույց տալ փոփոխությունը (debug-ի համար)
 	console.log(`Transition duration set to: ${duration}s`);
 }
-//*	<!-- /////////////////////// //////////////////////////// ///////// -->*/	
+//*	<!-- /////////////////////// //////////////////////////// ///////// -->*/
+
+document.getElementById('btn-start').addEventListener("click", startCountdownTimer);
+function startCountdownTimer()
+{
+	let targetDataControl  = document.getElementById("target-date")
+	let targetTimeControl = document.getElementById("target-time")
+	let btnStart = document.getElementById('btn-start');
+	if (btnStart.value === "Start")
+	{
+		btnStart.value = "Stop";
+		targetDataControl.disabled = targetTimeControl.disabled = true;
+		tickCountdown();
+	}
+	else 
+	{
+		btnStart.value = "Start";
+		targetDataControl.disabled = targetTimeControl.disabled = false;
+		//clearTimeout(tickCountdown);
+	}
+}
+function tickCountdown()
+{
+	if (document.getElementById("btn-start").value === "Start") return;
+	let now = new Date();
+
+	let targetDate = document.getElementById("target-date").valueAsDate;
+	let targetTime = document.getElementById("target-time").valueAsDate;
+
+	//
+	targetDate.setHours(targetDate.getHours() + targetDate.getTimezoneOffset() / 60);
+	targetTime.setHours(targetTime.getHours() + targetTime.getTimezoneOffset() / 60);
+
+	//
+	targetTime.setFullYear(targetDate.getFullYear());
+	targetTime.setMonth(targetDate.getMonth());
+	targetTime.setDate(targetDate.getDate());
+
+	let timestamp = targetTime - now;
+	let duration = Math.trunc(timestamp / 1000);	//Truncation
+
+	document.getElementById("target-date-value").innerHTML = targetDate;
+	document.getElementById("target-time-value").innerHTML = targetTime;
+	document.getElementById("timezone").innerHTML = targetTime.getTimezoneOffset();
+	document.getElementById("duration").innerHTML = duration;
+	document.getElementById("timestamp").innerHTML = timestamp;
+
+	const SECONDS_PER_MINUTE = 60;
+	const SECONDS_PER_HOUR = 3600;
+	const SECONDS_PER_DAY = 86400;
+
+	let time_of_day = duration % SECONDS_PER_DAY;
+
+	document.getElementById("hours-unit").innerHTML = Math.trunc(time_of_day / SECONDS_PER_HOUR);
+	time_of_day = time_of_day % SECONDS_PER_HOUR;
+	document.getElementById("minutes-unit").innerHTML = Math.trunc(time_of_day / SECONDS_PER_MINUTE);
+	document.getElementById("seconds-unit").innerHTML = time_of_day % SECONDS_PER_MINUTE;
+
+	setTimeout(tickCountdown, 100);
+}
